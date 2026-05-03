ba8541b6430a837f920fb299bc9ee7d687134c12
Nozomu Kuwae <nozomukuwae@gmail.com>
2026-05-03 21:07:55 +0900

    Implement POST request introducing BookRequest model

--- books2.py
+++ books2.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -18,6 +19,14 @@ class Book:
         self.rating = rating
 
 
+class BookRequest(BaseModel):
+    id: int
+    title: str
+    author: str
+    description: str
+    rating: int
+
+
 BOOKS = [
     Book(1, 'Computer Science Pro', 'codingwithroby', 'A very nice book!', 5),
     Book(2, 'Be Fast with FastAPI', 'codingwithroby', 'A great book!', 5),
@@ -31,3 +40,10 @@ BOOKS = [
 @app.get("/books")
 async def read_all_books():
     return BOOKS
+
+
+@app.post("/create-book")
+async def create_book(book_request: BookRequest):
+    new_book = Book(**book_request.model_dump())
+    print(type(new_book))
+    BOOKS.append(new_book)
