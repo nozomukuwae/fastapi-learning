19630e6dda4d1cc65f361fcd2c3d3160a2b595b0
Nozomu Kuwae <nozomukuwae@gmail.com>
2026-05-03 19:11:05 +0900

    Implement query parameter

--- books.py
+++ books.py
@@ -11,9 +11,11 @@ BOOKS = [
     {'title': 'Title Six', 'author': 'Author Two', 'category': 'math'}
 ]
 
+
 @app.get("/books")
 async def read_all_books():
     return BOOKS
+
 
 @app.get("/books/{title}")
 async def read_book(title: str):
@@ -21,3 +23,14 @@ async def read_book(title: str):
         (book for book in BOOKS if book['title'].casefold() == title.casefold()),
         None
     )
+
+
+@app.get("/books/")
+async def read_category_by_query(category: str):
+    return [book for book in BOOKS if book['category'].casefold() == category.casefold()]
+
+
+@app.get("/books/{author}/")
+async def read_author_category_by_query(author: str, category: str):
+    return [book for book in BOOKS if
+            book['author'].casefold() == author.casefold() and book['category'].casefold() == category.casefold()]
