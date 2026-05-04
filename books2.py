e7bf4730b782b78ff60aaf534d8183284093f7f6
Nozomu Kuwae <nozomukuwae@gmail.com>
2026-05-04 12:43:21 +0900

    Add JSON schema example

--- books2.py
+++ books2.py
@@ -22,11 +22,22 @@ class Book:
 
 
 class BookRequest(BaseModel):
-    id: Optional[int] = None
+    id: Optional[int] = Field(description='ID is not needed on create', default=None)
     title: str = Field(min_length=3)
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(ge=1, le=5)
+
+    model_config = {
+        "json_schema_extra": {
+            "example": {
+                "title": "A new book",
+                "author": "Nozomu Kuwae",
+                "description": "A description of a new book",
+                "rating": 5
+            }
+        }
+    }
 
 
 BOOKS = [
