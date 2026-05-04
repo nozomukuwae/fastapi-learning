4bda15f7257aff359c55e3a889d2da1bf9157122
Nozomu Kuwae <nozomukuwae@gmail.com>
2026-05-04 15:54:23 +0900

    Add published year field

--- books2.py
+++ books2.py
@@ -12,13 +12,15 @@ class Book:
     author: str
     description: str
     rating: int
+    published_year: int
 
-    def __init__(self, id: int, title: str, author: str, description: str, rating: int):
+    def __init__(self, id: int, title: str, author: str, description: str, rating: int, published_year: int):
         self.id = id
         self.title = title
         self.author = author
         self.description = description
         self.rating = rating
+        self.published_year = published_year
 
 
 class BookRequest(BaseModel):
@@ -27,6 +29,7 @@ class BookRequest(BaseModel):
     author: str = Field(min_length=1)
     description: str = Field(min_length=1, max_length=100)
     rating: int = Field(ge=1, le=5)
+    published_year: int = Field(ge=2000, le=2100)
 
     model_config = {
         "json_schema_extra": {
@@ -34,19 +37,20 @@ class BookRequest(BaseModel):
                 "title": "A new book",
                 "author": "Nozomu Kuwae",
                 "description": "A description of a new book",
-                "rating": 5
+                "rating": 5,
+                "published_year": 2026
             }
         }
     }
 
 
 BOOKS = [
-    Book(1, 'Computer Science Pro', 'codingwithroby', 'A very nice book!', 5),
-    Book(2, 'Be Fast with FastAPI', 'codingwithroby', 'A great book!', 5),
-    Book(3, 'Master Endpoints', 'codingwithroby', 'A awesome book!', 5),
-    Book(4, 'HP1', 'Author 1', 'Book Description', 2),
-    Book(5, 'HP2', 'Author 2', 'Book Description', 3),
-    Book(6, 'HP3', 'Author 3', 'Book Description', 1)
+    Book(1, 'Computer Science Pro', 'codingwithroby', 'A very nice book!', 5, 2012),
+    Book(2, 'Be Fast with FastAPI', 'codingwithroby', 'A great book!', 5, 2020),
+    Book(3, 'Master Endpoints', 'codingwithroby', 'A awesome book!', 5, 2024),
+    Book(4, 'HP1', 'Author 1', 'Book Description', 2, 2026),
+    Book(5, 'HP2', 'Author 2', 'Book Description', 3, 2013),
+    Book(6, 'HP3', 'Author 3', 'Book Description', 1, 1999)
 ]
 
 
@@ -65,6 +69,11 @@ async def read_book(book_id: int):
 @app.get("/books/")
 async def read_books_by_rating(rating: int):
     return [book for book in BOOKS if book.rating == rating]
+
+
+@app.get("/books/by_year/")
+async def read_books_by_published_year(year: int):
+    return [book for book in BOOKS if book.published_year == year]
 
 
 @app.post("/create-book")
