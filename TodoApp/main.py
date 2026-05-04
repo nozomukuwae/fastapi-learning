627412d9b4d034b0a3f6b68e7927779d85554744
Nozomu Kuwae <nozomukuwae@gmail.com>
2026-05-04 22:00:36 +0900

    Implement PUT request

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -49,3 +49,14 @@ async def create_todo(db: db_dependency, todo_request: TodoRequest):
     todo = Todos(**todo_request.model_dump())
     db.add(todo)
     db.commit()
+
+
+@app.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def update_todo(db: db_dependency, todo_request: TodoRequest, todo_id: int = Path(gt=0)):
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    if todo_model is None:
+        raise HTTPException(status_code=404, detail='Todo not found.')
+
+    for key, value in todo_request.model_dump().items():
+        setattr(todo_model, key, value)
+    db.commit()
