f0ea77ce93ed76f1e72a1e82eccf70f2738fe7de
Nozomu Kuwae <nozomukuwae@gmail.com>
2026-05-05 16:27:26 +0900

    Save user to database

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -1,8 +1,48 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+from typing import Annotated
+from sqlalchemy.orm import Session
+from database import SessionLocal
+from starlette import status
+from models import Users
+import bcrypt
 
 router = APIRouter()
 
 
-@router.get("/auth/")
-async def get_user():
-    return {'user': 'authenticated'}
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+class CreateUserRequest(BaseModel):
+    email: str
+    username: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+
+@router.post("/auth", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
+    user = Users(
+        email=create_user_request.email,
+        username=create_user_request.username,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        hashed_password=bcrypt.hashpw(
+            create_user_request.password.encode('utf-8'),
+            bcrypt.gensalt()
+        ).decode('utf-8'),
+        is_active=True,
+        role=create_user_request.role,
+    )
+    db.add(user)
+    db.commit()
