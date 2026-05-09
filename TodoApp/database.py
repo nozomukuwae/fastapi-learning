fcff49ff8ff7304381233c693979c9dc0c4cd50a
Nozomu Kuwae <nozomukuwae@gmail.com>
2026-05-09 19:30:27 +0900

    Connect to MySQL

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -7,7 +7,7 @@ from sqlalchemy.ext.declarative import declarative_base
 
 load_dotenv()
 DATABASE_PASSWORD = os.environ['DATABASE_PASSWORD']
-SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:' + DATABASE_PASSWORD + '@localhost/TodoApplicationDatabase'
+SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:' + DATABASE_PASSWORD + '@127.0.0.1:3306/TodoApplicationDatabase'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
