7631e9a4f4c3bd3c967c82c4010eb2877e2447e7
Nozomu Kuwae <nozomukuwae@gmail.com>
2026-05-09 18:01:19 +0900

    Connect to PostgreSQL

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -1,10 +1,15 @@
+import os
+
+from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'
+load_dotenv()
+DATABASE_PASSWORD = os.environ['DATABASE_PASSWORD']
+SQLALCHEMY_DATABASE_URL = 'postgresql://postgres:' + DATABASE_PASSWORD + '@localhost/TodoApplicationDatabase'
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
