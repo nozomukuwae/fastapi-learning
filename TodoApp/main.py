2fdc5c6b8e75d3a263877dd62217fa2f64ce2aba
Nozomu Kuwae <nozomukuwae@gmail.com>
2026-05-23 21:23:07 +0900

    Create basic test

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -7,6 +7,12 @@ app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
 
+
+@app.get("/healthy")
+def health_check():
+    return {'status': 'healthy'}
+
+
 app.include_router(auth.router)
 app.include_router(todos.router)
 app.include_router(admin.router)
